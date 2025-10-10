5a730ccd23cfb81970489024a9e30164d4ca8943
Dash56 <aivideoscreateone@gmail.com>
2025-10-11 01:22:45 +0400

    Updated DepartmentsPage.jsx

--- src/pages/DepartmentsPage.jsx
+++ src/pages/DepartmentsPage.jsx
@@ -151,7 +151,7 @@ export default function DepartmentsPage() {
 
   return (
     <div className="p-6">
-      
+      <Sidebar />
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-2xl font-bold">Departamentlər</h1>
         <button
