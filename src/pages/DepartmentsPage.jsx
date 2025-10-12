e9579062de3739d6ca99f1c58985575cbfbf56e5
Dash56 <aivideoscreateone@gmail.com>
2025-10-12 21:47:42 +0400

    Updated DepartmentsPage.jsx

--- src/pages/DepartmentsPage.jsx
+++ src/pages/DepartmentsPage.jsx
@@ -157,7 +157,7 @@ export default function DepartmentsPage() {
       <Header
 					title='Şöbələr'
 				/>
-      <div>
+      <div className="p-6">
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-2xl font-bold">Departamentlər</h1>
         <button
