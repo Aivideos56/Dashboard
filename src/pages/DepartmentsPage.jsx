d59ba90d5ea720156f35b1449022ae41d0d5543a
Dash56 <aivideoscreateone@gmail.com>
2025-10-12 21:38:53 +0400

    Updated DepartmentsPage.jsx

--- src/pages/DepartmentsPage.jsx
+++ src/pages/DepartmentsPage.jsx
@@ -157,7 +157,7 @@ export default function DepartmentsPage() {
       <Header
 					title='Şöbələr'
 				/>
-      <div className="flex justify-between items-center mb-4 p-6">
+      <div className="flex justify-between items-center mb-4 ">
         <h1 className="text-2xl font-bold">Departamentlər</h1>
         <button
           onClick={() => { setSelectedDept(null); setModalOpen(true); }}
