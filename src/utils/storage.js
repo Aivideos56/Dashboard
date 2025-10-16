59c752af6ce801224cd9a55e87fca3c9d9dbae87
Dash56 <aivideoscreateone@gmail.com>
2025-10-17 03:31:13 +0400

    Updated storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -8,6 +8,7 @@ const STORAGE_KEYS = {
 };
 
 export const setUser = (userData, type = 'restaurant') => {
+  console.log(userData)
 	try {
 		if (type === 'admin') {
 			localStorage.setItem('admin_user', JSON.stringify(userData));
