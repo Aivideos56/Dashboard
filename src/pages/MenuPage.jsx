326503f46d7a25756030742e05272c26173816f0
Dash56 <aivideoscreateone@gmail.com>
2025-10-20 22:57:16 +0400

    Updated MenuPage.jsx

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -44,7 +44,7 @@ export default function MenuPage() {
 	}, [restaurant]);
 
 	const loadData = async () => {
-		const [categoriesData, productsData] = await Promise.all([storageUtils.getCategories(restaurant.id), storageUtils.getProducts(restaurant.id)]);
+		const [categoriesData, productsData] = await Promise.all([storageUtils.getCategories(restaurant.id), storageUtils.getProducts(restaurant.id), storageUtils.getDepartments(restaurant.id)]);
 
 		dispatch(setCategories(categoriesData));
 		dispatch(setProducts(productsData));
