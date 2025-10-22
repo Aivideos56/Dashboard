ae64546cdc5a921645a235edba97e44c810e8e98
Dash56 <aivideoscreateone@gmail.com>
2025-10-22 07:28:56 +0400

    Updated ModifiersPage.jsx

--- src/pages/ModifiersPage.jsx
+++ src/pages/ModifiersPage.jsx
@@ -99,7 +99,7 @@ export default function ModifiersPage({ restaurantId }) {
       <Sidebar />
     <div className="flex h-screen bg-gray-50">
         <Header
-					title='Modffikatorlar'
+					title='Modifikatorlar'
 				/>  
       <div className="flex-1 overflow-y-auto p-6">
         <div className="max-w-7xl mx-auto">
